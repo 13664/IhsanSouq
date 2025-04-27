cf70a8eefbcf58ed3ffe5247fe804d471a0dfc4b
13664 <wiut13664@gmail.com>
2025-04-27 11:21:45 +0500

    homepage

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,10 +6,11 @@ import { CharityCase } from './shared/models/charityCase';
 import { Pagination } from './shared/models/pagination';
 import { PortalService } from './core/services/portal.service';
 import { PortalComponent } from "./features/portal/portal.component";
+import { FooterComponent } from "./layout/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, PortalComponent],
+  imports: [RouterOutlet, HeaderComponent, PortalComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
